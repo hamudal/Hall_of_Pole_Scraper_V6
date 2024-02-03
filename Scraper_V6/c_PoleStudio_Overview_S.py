1dbe967599fa3bc8256de03dccde818dd7012c8d
Muhammed Al-Dalqamouni <hamudal.hd@gmail.com>
2024-02-03 16:27:32 +0100

    Fixed Whitespaces Pole Studio Übersicht

--- Scraper_V6/c_PoleStudio_Overview_S.py
+++ Scraper_V6/c_PoleStudio_Overview_S.py
@@ -27,11 +27,11 @@ def get_soup(url):
 # Extraction functions
 def extract_overview_buttons(soup):
     buttons = soup.find_all('div', class_="MuiStack-root css-sgccrm")
-    return [button.text for div in buttons for button in div.find_all('a')]
+    return [button.text.rstrip() for div in buttons for button in div.find_all('a')]
 
 def extract_pole_studio_name(soup):
     name_element = soup.find('h1', class_='MuiTypography-root MuiTypography-h1 css-l64ylu')
-    return name_element.text if name_element else None
+    return name_element.text.rstrip() if name_element else None
 
 def extract_contact_info(soup):
     contact_divs = soup.find_all('div', class_='css-1x2phcg')
@@ -40,18 +40,18 @@ def extract_contact_info(soup):
         for a in div.find_all('a', href=True):
             href = a['href']
             if href.startswith('mailto:'):
-                contact_info["E-Mail"] = href.replace('mailto:', '')
+                contact_info["E-Mail"] = href.replace('mailto:', '').rstrip()
             elif href.startswith('tel:'):
-                contact_info["Telefon"] = href.replace('tel:', '')
+                contact_info["Telefon"] = href.replace('tel:', '').rstrip()
             else:
-                contact_info["Homepage"] = href
+                contact_info["Homepage"] = href.rstrip()
     return contact_info
 
 def extract_address(soup):
     address_element = soup.find('p', class_='MuiTypography-root MuiTypography-body1 css-1619old')
     if address_element:
         address_text = address_element.text.split(',')
-        return address_text, address_text[1].split(" ")[2], address_text[1].split(" ")[1], address_text[0]
+        return address_text, address_text[1].split(" ")[2].rstrip(), address_text[1].split(" ")[1].rstrip(), address_text[0].rstrip()
     return None, None, None, None
 
 def extract_description(soup):
@@ -63,29 +63,13 @@ def extract_description(soup):
     else:
         return None
 
-
-# def extract_rating(soup):
-#     rating_element = soup.find('p', class_='MuiTypography-root MuiTypography-body1 css-2g7rhg')
-#     if rating_element:
-#         parts = rating_element.text.split('(')
-#         return parts[0].strip(), parts[1].replace(')', '').strip()
-#     return None, None
-
-# def extract_rating_factors(soup):
-#     items = soup.find_all('div', class_='MuiStack-root css-95g4uk')
-#     return [f"{item.find('p', class_='MuiTypography-root MuiTypography-body1 css-1k55edk').text}: {item.find('p', class_='MuiTypography-root MuiTypography-body1 css-1y0caop').text}" for item in items if item.find('p', class_='MuiTypography-root MuiTypography-body1 css-1k55edk') and item.find('p', class_='MuiTypography-root MuiTypography-body1 css-1y0caop')]
-
 def extract_art(soup):
     art_elements = soup.find_all("p", class_="MuiTypography-root MuiTypography-body1 css-6ik050")
-    return [art.text for art in art_elements]
+    return [art.text.rstrip() for art in art_elements]
 
 def extract_sale(soup):
     sale_element = soup.find("p", class_="MuiTypography-root MuiTypography-body1 css-153qxhx")
-    return sale_element.text if sale_element else None
-
-# def extract_image_urls(soup):
-#     pictures = soup.find_all("div", class_="MuiBox-root css-1fivxf")
-#     return [img["src"] for div in pictures if (img := div.find("img")) and img.has_attr("src")]
+    return sale_element.text.rstrip() if sale_element else None
 
 # Main function scrape_pole_studio
 def scrape_pole_studio(url):
@@ -108,11 +92,8 @@ def scrape_pole_studio(url):
     contact_info = extract_contact_info(polestudio_soup)
     address, town, postal_code, street = extract_address(polestudio_soup)
     description_text = extract_description(polestudio_soup)
-    # rating, num_reviews = extract_rating(polestudio_soup)
-    # combined_items = extract_rating_factors(polestudio_soup)
     arten = extract_art(polestudio_soup)
     sale = extract_sale(polestudio_soup)
-    # image_urls = extract_image_urls(polestudio_soup)
 
     # Creating the dictionary and returning it as a DataFrame
     pole_studio_overview = {
@@ -123,14 +104,12 @@ def scrape_pole_studio(url):
         'Straße': street,
         'Buttons': overview_buttons,
         'Pole Studio Beschreibung': description_text,
-        #'Rating Faktoren': combined_items,
         'E-Mail': contact_info['E-Mail'],
         'Homepage': contact_info['Homepage'],
         'Telefon': contact_info['Telefon'],
         'URL_S': url,
         'Art': arten,
         'Angebot': sale,
-        #'Bildergalerie': image_urls
     }
 
     pole_studio_df = pd.DataFrame([pole_studio_overview])
